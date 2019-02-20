7d541a2990f3e6741fe78082ba2a7bd0e397ffa8
manika1994 <manika.vishnoi@gmail.com>
2019-02-20 16:12:18 +0530

    POC1 for multiselect at autotext

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'POC1';
+    public listItems: Array<{ text: string, value: number }> = [
+        { text: "Small", value: 1 },
+        { text: "Medium", value: 2 },
+        { text: "Large", value: 3 }
+    ];
+    public value: any = [{ text: "Medium", value: 2 }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { NgModule }      from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import 'hammerjs';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ FormsModule, BrowserModule, BrowserAnimationsModule, DropDownsModule, DateInputsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
+
+
 export class AppModule { }
